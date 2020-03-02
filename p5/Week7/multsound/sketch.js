86cbc2e9a2595056f952e148929ef6c5c5ea7c0f
Jakeh22 <59931506+Jakeh22@users.noreply.github.com>
2020-03-01 21:14:46 -0600

    ds

--- p5/Week7/multsound/sketch.js
+++ p5/Week7/multsound/sketch.js
@@ -1,7 +1,32 @@
+var song1;
+
+function preload() {
+  song1 = loadSound('assets/JOSH_J_guitar_loop_spooky_150_Enm.wav');
+}
+
 function setup() {
-  // put setup code here
+
+  createCanvas(720, 200);
+  background(255, 0, 0);
+  song1.play(); // this is what starts the sound
+
 }
 
 function draw() {
-  // put drawing code here
+
+}
+
+function mouseReleased() {
+  if (song1.isPlaying()) {
+    song1.pause();
+  } else {
+    song1.play() ;
+  }
+
+
+}
+
+
+function touchStarted() {
+  getAudioContext().resume();
 }
